70fe7a608cb73f6b4c08842668164795e7400404
Yonel Ceruto <yonel.ceruto@scnd.com>
2023-09-28 12:58:29 -0400

    simplify money payload

--- src/Shared/Presentation/Request/Money/MoneyPayload.php
+++ src/Shared/Presentation/Request/Money/MoneyPayload.php
@@ -4,13 +4,16 @@ namespace App\Shared\Presentation\Request\Money;
 
 use App\Shared\Presentation\OpenApi\Attributes\Property;
 use App\Shared\Presentation\OpenApi\Attributes\Schema;
-use Symfony\Component\Validator\Constraints as Assert;
 
 #[Schema]
 class MoneyPayload
 {
-    #[Property(description: 'Amount in cents', example: 100, groups: ['Money'])]
-    #[Assert\PositiveOrZero(groups: ['Money'])]
+    #[Property(
+        description: 'Amount in cents',
+        minimum: 0,
+        example: 100,
+        groups: ['Money'],
+    )]
     public int $amount;
 
     #[Property(
